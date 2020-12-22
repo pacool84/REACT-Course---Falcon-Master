8494f27dd15e087c9f79585415976407bff76eac
Francisco López <flopez_1384@hotmail.com>
2020-12-22 12:45:23 -0600

    [ADD] JSX code to render content base on IF conditional

--- using_create_react_app/src/index.js
+++ using_create_react_app/src/index.js
@@ -2,17 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const name = "Paco";
-const setColor = "red";
-const setColor2 = "blue";
+const setColor = "green";
+const setColor2 = "red";
+const sesion = true;
+
 const codeJsx = (
   <div>
     <h1 className='encabezado' style={{ color: setColor }}>
-      Hello my friend REACT, from your dear {name}
+      Hello my friend REACT, You initialize sesion with the id {name}
     </h1>
-    <p className='parrafo' style={{ color: setColor2 }}>
-      Remember to render only one element
-    </p>
   </div>
 );
 
-ReactDOM.render(codeJsx, document.getElementById("root"));
+const verificarSesion = () => {
+  if (sesion) {
+    return codeJsx;
+  } else {
+    return <h1 style={{ color: setColor2 }}>No has iniciado la sesion :(</h1>;
+  }
+};
+
+ReactDOM.render(verificarSesion(sesion), document.getElementById("root"));
