9cfcefdc4124766f6e8eea0dd41bcdda5247d52d
Francisco López <flopez_1384@hotmail.com>
2020-12-22 12:27:46 -0600

    [ADD] JSX language to the project to learn the concepts of use JS and HTML

--- using_create_react_app/src/index.js
+++ using_create_react_app/src/index.js
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <h1>Hello my friend REACT</h1>
-  </React.StrictMode>,
-  document.getElementById("root")
+const name = "Paco";
+const setColor = "red";
+const setColor2 = "blue";
+const codeJsx = (
+  <div>
+    <h1 className='encabezado' style={{ color: setColor }}>
+      Hello my friend REACT, from your dear {name}
+    </h1>
+    <p className='parrafo' style={{ color: setColor2 }}>
+      Remember to render only one element
+    </p>
+  </div>
 );
+
+ReactDOM.render(codeJsx, document.getElementById("root"));
