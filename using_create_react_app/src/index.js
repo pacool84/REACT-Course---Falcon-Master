8b52533f3cc2ad2424fe2ad42ef5bf87d2565c1d
Francisco López <flopez_1384@hotmail.com>
2020-12-22 12:54:03 -0600

    [ADD] Ternary operator in order to render content base on a condition

--- using_create_react_app/src/index.js
+++ using_create_react_app/src/index.js
@@ -4,22 +4,28 @@ import ReactDOM from "react-dom";
 const name = "Paco";
 const setColor = "green";
 const setColor2 = "red";
-const sesion = true;
+const sesion = false;
 
 const codeJsx = (
-  <div>
-    <h1 className='encabezado' style={{ color: setColor }}>
-      Hello my friend REACT, You initialize sesion with the id {name}
-    </h1>
-  </div>
+  <>
+    {sesion === true ? (
+      <div>
+        <h1 className='encabezado' style={{ color: setColor }}>
+          Hello my friend REACT, You initialize sesion with the id {name}
+        </h1>
+      </div>
+    ) : (
+      <h1 style={{ color: setColor2 }}>No has iniciado sesion :(</h1>
+    )}
+  </>
 );
 
-const verificarSesion = () => {
+/* const verificarSesion = () => {
   if (sesion) {
     return codeJsx;
   } else {
     return <h1 style={{ color: setColor2 }}>No has iniciado la sesion :(</h1>;
   }
 };
-
-ReactDOM.render(verificarSesion(sesion), document.getElementById("root"));
+ */
+ReactDOM.render(codeJsx, document.getElementById("root"));
